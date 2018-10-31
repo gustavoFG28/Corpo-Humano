9bf3e89857da7f887cbba6f03388b3d2db0d600d
Gustavo Gitzel <42344174+gustavoFG28@users.noreply.github.com>
2018-10-31 13:34:45 -0300

    Corrigindo servidor do node

--- Pagina/nodeSQL/appSQL.js
+++ Pagina/nodeSQL/appSQL.js
@@ -3,7 +3,7 @@ const app = express();					// objeto do tipo express - acessar o banco
 const bodyParser = require('body-parser');		// variavel associada com o modulo do pacote body-parser
 const porta = 3000;						 //porta padrão - passar na url para acessar a aplicação
 const sql = require('mssql');			// variavel que usa as funções do pacote mssql
-const conexaoStr = "Server=regulus.cotuca.unicamp;Database=PR118194;User Id=PR118194;Password=guisa2018;";	// string necessária para conectar-se ao banco
+const conexaoStr = "Server=regulus;Database=PR118194;User Id=PR118194;Password=guisa2018;";	// string necessária para conectar-se ao banco
 
 //conexao com BD
 sql.connect(conexaoStr)	// sql é instancia, connect conecta ao banco q tem como param a string
